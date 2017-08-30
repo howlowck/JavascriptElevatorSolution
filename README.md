358e085a99326669b24da9380c6b36fce0a9b7bb
Jennifer Marsman <jennmar@microsoft.com>
2017-08-30 09:25:23 -0400

    Added dependency on tostr, more sample code in elevator.js to get started, and simplified the challenge in the readme

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ as a separate thread, but remember that JavaScript is single-threaded.)
 
 The format of the text file (elevatorRequests.txt in this directory) is 
 as follows: 
-> NumPassengers FromFloor ToFloor
+> FromFloor ToFloor
 
-So the line "2 1 5" means that 2 people are on the first floor (since
+So the line "1 5" means that people are on the first floor (since
 buildings don't typically zero-index their floors) and want to go up to 
 the fifth floor.  
 
@@ -20,7 +20,17 @@ method parameters, and change data structures.
 
 In order to run all the code include the async/wait, you'll need Node 7+.
 
-Getting Started:
-1. run `yarn` or `npm install`
-2. Make sure you have the Current version of node installed (7+)
-3. Run the js files in your console.
+##Getting Started
+1. Run `yarn` or `npm install`.
+2. Make sure you have the current version of node installed (7+).
+3. Run `node elevator.js` in your console.
+
+##Tasks
+1. Read from the `elevatorRequests.txt` file asynchronously.  
+2. For each elevator request, allocate an elevator.  There is a time delay associated with each elevator's movement between floors, so try to do this efficiently as well.  
+
+##Optional Tasks
+3. Measure the time each elevator ride takes, and calculate the average time and total time for all rides.  
+4. Create an object to store state for each elevator (such as current position) so that they don't ever disappear from one place and appear in another place.  
+5. Experiment with more efficient elevator scheduling algorithms (to stop on multiple floors to pick up people going in the same direction).  
+6. Enforce a 12-person limit on each elevator.  
--- elevator.js
+++ elevator.js
@@ -1,69 +1,83 @@
-var puts = require('./lib/puts')
-var gets = require('./lib/gets')
+var tostr = require('tostr')
+var timePerFloor = 25;  // in milliseconds
+var numElevators = 32;
+var elevatorInUse = [];
+//var iterator;
 
-var maxPeople = 12;
-var totalTime = 0;
-
-var elevator = {
-    currentFloor:0,
-    numPassengers:0
-}; 
-
-var passenger = {
-    startTime:0,
-    endTime:0, 
-    startFloor:0,
-    endFloor:0
-};
-
-// TODO: elevators data structure - array of 32 elevator objects?  
-// TODO: assume elevators all start at floor 1?  
+function initializeElevators() {
+    for (var i = 0; i <= numElevators; i++)
+    {
+        elevatorInUse[i] = 0;   // Not in use
+    }
+}
 
 function * readElevatorRequestFromFile () {
     var lineReader = require('readline').createInterface({
         input: require('fs').createReadStream('elevatorRequests.txt')
     });
-    console.log('Did init work? ');
+
     lineReader.on('line', function(line) {
         console.log('Line from file: ', line);
-        yield line;
     });
 }
 
 function run() {
-    var iterator = readElevatorRequestFromFile();
+    console.log('Beginning the elevator challenge - run 1');
+    var iterator = readElevatorRequestFromFile();   // TODO: need to read async
     var request = iterator.next();
+    console.log(tostr(request.done));   // This value is a hint.  
 
-    while (!request.done)   // TODO: is that legal in JS?  
+    while (request.done === false)
     {
+        console.log('inside while loop');
+        // Parsing out the request
         var items = request.value.split(" ");
         var toFloor = items[0];
         var fromFloor = items[1];
-        console.log("Processing request from floor " + str(fromFloor) + " to floor " + str(toFloor) );
+        console.log("Processing request from floor " + tostr(fromFloor) + " to floor " + tostr(toFloor) );
         request = iterator.next();
     }
 }
 
-// Parsing out the request
-function processLine(line) {
-    var items = line.split(" ");
-    //var nu
+function run2() {
+    console.log('Beginning the elevator challenge - run 2');
+    iterator = readElevatorRequestFromFile();   // TODO: need to read async
+
+    for (var i = 0; i <= numElevators; i++)
+    {
+        allocateElevator(i);
+    }
 }
 
-function incrementTotalTime(time) {
-    totalTime += time;
-}
-
-function findElevator(elevators) {
-    // TODO: find the appropriate elevator to get passengers
+function allocateElevator(elevatorNum) {
+    var request = iterator.next();
+    if (!request.done)
+    {
+        // Parsing out the request
+        var items = request.value.split(" ");
+        var fromFloor = items[0];
+        var toFloor = items[1];
+        elevatorInUse[elevatorNum] = 1;
+        moveElevator(elevatorNum, fromFloor, toFloor);
+        request = iterator.next();
+    }
+    else {
+        console.log("Done!");
+    }
 }
 
 function moveElevator(elevator, fromFloor, toFloor) {
-
+    console.log("Moving elevator " + elevator + " from " + str(fromFloor) + " to " + str(toFloor));
+    
+    // Start timer based on the distance between floors
+    var distance = Math.abs(fromFloor - toFloor);
+    setTimeout(elevatorArrived, distance * timePerFloor);
 }
 
-function getOn(elevator) {
-    // 
+function elevatorArrived(elevatorNum) {
+    elevatorInUse[elevatorNum] = 0;     // available again, so reassign
+    allocateElevator(elevatorNum);
 }
 
-run();
+run();      // or you are welcome to start with run2 sample code if you prefer
+
